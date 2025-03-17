2bda4b10185069999ec70256dc8440395cd6dbe9
Munimbaig024 <munimbaig12345@gmail.com>
2025-03-17 11:40:30 +0500

    Manager Login/Dashboard

--- src/index.js
+++ src/index.js
@@ -4,11 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter ,
+  RouterProvider,
+  Route , 
+  
+
+} from "react-router-dom";
+
+const router = createBrowserRouter ([
+  {
+    path:"/",
+    element:<App/>
+
+  }
+]);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <RouterProvider router = {router} />
+  // </React.StrictMode>
+
+  <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
