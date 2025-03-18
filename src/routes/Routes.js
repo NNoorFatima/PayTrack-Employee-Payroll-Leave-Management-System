dfb20fe7ff87f1615208aa3cf37d6d8b3dd87f16
NNoorFatima <noor.2205.nf@gmail.com>
2025-03-18 05:34:18 +0500

    login + dashboard of manager done

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -12,6 +12,10 @@ import AddManager from "../pages/Administrator/AddManager";
 import RemoveHr from "../pages/Administrator/RemoveHR";
 import RemoveManager from "../pages/Administrator/RemoveManager";
 import AdminLogin from "../pages/Administrator/AdminLogin";
+
+import Dashboard from "../pages/Manager/Dashboard";
+import ManagerLogin from "../pages/Manager/ManagerLogin";
+
 const AppRoutes = () => {
   return (
     <Routes>
@@ -21,13 +25,16 @@ const AppRoutes = () => {
       <Route path="/leave-request" element={<MakeLeaveRequest />} />
 
 
-      <Route path="/" element={<AdminDashboard />} /> ✅ Default page is now Admin.js
+      {/* <Route path="/" element={<AdminDashboard />} /> ✅ Default page is now Admin.js */}
       <Route path="/admin" element={<AdminDashboard />} />
       <Route path="/add-hr" element={<AddHr />} />
       <Route path="/add-manager" element={<AddManager />} />
       <Route path="/remove-hr" element={<RemoveHr />} />
       <Route path="/remove-manager" element={<RemoveManager />} />
       <Route path="/admin-login" element={<AdminLogin />} />
+
+      <Route path="/" element={<Dashboard />} />
+      {/* <Route path="/" element={<ManagerLogin />} /> */}
     </Routes>
   );
 };
