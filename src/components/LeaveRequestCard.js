2a7fe6b466a406ba3802bde9c06b8c908d499e67
NNoorFatima <noor.2205.nf@gmail.com>
2025-04-13 15:43:41 +0500

    added icons/badges for status of leave approval

--- src/components/LeaveRequestCard.js
+++ src/components/LeaveRequestCard.js
@@ -1,12 +1,38 @@
 // src/components/LeaveRequestCard.js
 import React from "react";
+import { Badge } from "./badge";
+
 
 const LeaveRequestCard = ({ request, formatDate }) => {
+  const getStatusIcon = (status) => {
+    switch (status.toLowerCase()) {
+      case "approved":
+        return "🎊";
+      case "pending":
+        return "👻";
+      case "rejected":
+        return "🤡";
+      default:
+        return "ℹ️";
+    }
+  };
+   
+
+
+
+
   return (
     <div className="leave-request-card">
       <h3>{request.type}</h3>
       <p>Leave Date: {formatDate(request.leaveDate)}</p>
-      <p>Status: <strong>{request.status}</strong></p>
+      {/* <p>Status: <strong>{request.status}</strong></p> */}
+      <p>
+        Status: <Badge className={request.status.toLowerCase()}>
+                  {getStatusIcon(request.status)} {request.status}
+                </Badge>
+      </p>
+
+
       <p>Reason: <i>{request.reason}</i></p>
       {request.status !== "pending" && (
         <>
